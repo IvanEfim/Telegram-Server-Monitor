6a15c7715caea89525ff77f2c4ba9fac2d56b418
IvanEfim <49581403+IvanEfim@users.noreply.github.com>
2023-06-07 07:37:58 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+
+
+# Название телеграмм бота
+NAME = "monitoradmin_Bot"
+
+# токен
+TOKEN = "6101499582:AAHYSe3K0MjLcj5S9anXFnEHI9ZVnr8-lEs"
+
+# пароль"/старт <пароль>"
+PASSWORD = "qw12er13"
+
+# продолжительность ожидания в секундах
+TIMEOUT = 60 * 5
+
+# Уыедомления 
+ENABLE_NOTIFICATIONS = True
+
+# Интервалл секунд сообщений
+NOTIFCATION_INTERVAL = 10
+
+# Максимум загрузка ЦП 
+NOTIFY_CPU_PERCENT = 10
+
+# Максимум загрузка память
+NOTIFY_RAM_PERCENT = 10
+
+# DO NOT EDIT BELOW THIS LINE
+# ===========================
+API_URL = "https://api.telegram.org/bot" + TOKEN + "/"
